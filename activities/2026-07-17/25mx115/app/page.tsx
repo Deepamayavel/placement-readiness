3c083f6740cf79d1f9973102a9deb4c1e1772dae
krishnapriya094 <25mx115@psgtech.ac.in>
2026-07-18 11:29:22 +0530

    Add Mission 2 submission: Dynamic JSON Form Generator

---
+++ activities/2026-07-17/25mx115/app/page.tsx
@@ -0,0 +1,9 @@
+import DynamicForm from "@/components/DynamicForm";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-100 p-10">
+      <DynamicForm />
+    </main>
+  );
+};
